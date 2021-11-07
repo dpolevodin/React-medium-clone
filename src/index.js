d0ddaad981376c5be4de7a941b1fa88ee80667a2
dpolevodin <dpolevodin@gmail.com>
2021-11-07 15:36:47 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import Routes from '../src/node_modules/routes'
+import TopBar from 'components/topBar'
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
